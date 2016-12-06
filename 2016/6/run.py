23128917f1dd75fffbb0d660ab92e8b4f5a04107
tyler <tyhampjone@gmail.com>
2016-12-06 13:06:31 -0700

    2016 Day 6

--- 2016/6/run.py
+++ 2016/6/run.py
@@ -1,3 +1,26 @@
 from shared import *
+import string
 
 input_ary = read_input('input.txt')
+
+key = []
+for i in range(0, len(input_ary[0].rstrip())):
+    key.append({})
+
+for i in range(0, len(key)):
+    for x in string.ascii_lowercase:
+        key[i][x] = 0
+
+print key
+
+for line in input_ary:
+    s = line.rstrip()
+    for i in range(0,len(s)):
+        key[i][s[i]] +=1
+
+t = ''
+for c in key:
+    print c
+    # t+=max(c, key=lambda key: c[key])
+    t+=min(c, key=lambda key: c[key])
+print t
