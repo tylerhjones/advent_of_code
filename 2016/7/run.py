d3f1401eecaa1b2ac5993ea60a29b1e51c78b06d
tyler <tyhampjone@gmail.com>
2016-12-08 01:00:50 -0700

    2016 7 and 8

--- 2016/7/run.py
+++ 2016/7/run.py
@@ -1,3 +1,55 @@
 from shared import *
+import re
 
 input_ary = read_input('input.txt')
+
+def array_contains_abba(s_array):
+    for s in s_array:
+        if contains_abba(s):
+            return True
+    return False
+
+def contains_abba(s):
+    for i in range(0, len(s)-3):
+        if (s[i] == s[i+3] and s[i+1] == s[i+2] and s[i] != s[i+1]):
+            return True
+    return False
+
+total = 0
+for line in input_ary:
+    line = line.rstrip()
+    m = re.finditer('(\[[a-z]+\])', line)
+    s_array = [s.group(0) for s in m]
+    if not array_contains_abba(s_array):
+        split = re.split('\[[a-z]+\]', line)
+        if array_contains_abba(split):
+                total += 1
+
+# print total
+def has_converse(s):
+    return s[0] == s[2]
+
+def get_converse(s):
+    return s[1]+s[0]+s[1]
+
+def has_match(block_array, s_array):
+    for block in block_array:
+        for i in range(0, len(block)-3):
+            sub_string = block[i:i+3]
+            if has_converse(sub_string):
+                converse = get_converse(sub_string)
+                for s in s_array:
+                    if converse in s:
+                        return True
+    return False
+
+total = 0
+for line in input_ary:
+    line = line.rstrip()
+    m = re.finditer('(\[[a-z]+\])', line)
+    block_array = [s.group(0) for s in m]
+    s_array = re.split('\[[a-z]+\]', line)
+    if has_match(block_array, s_array):
+        total += 1
+
+print total
--- 2016/8/run.py
+++ 2016/8/run.py
@@ -1,3 +1,60 @@
 from shared import *
-
+import re
+import sys
 input_ary = read_input('input.txt')
+
+def transpose(a):
+    return map(list, zip(*a))
+
+a = []
+for i in range(0,6):
+    b = []
+    for j in range(0,50):
+        b.append(0)
+    a.append(b)
+
+def get_x_y(s):
+    m = s.split(' ')[1].split('x')
+    return int(m[0]), int(m[1])
+
+
+for line in input_ary:
+    line = line.rstrip()
+
+    if 'rect' in line:
+        x, y = get_x_y(line)
+
+        for i in range(0, y):
+            for j in range(0, x):
+                a[i][j] = 1
+    elif 'column' in line:
+        # rotate row y=2 by 48
+        m = line.split('=')[1].split(' by ')
+        row_index = int(m[0])
+        num = int(m[1])
+
+        a = transpose(a)
+
+        for i in range(0, num):
+            a[row_index].insert(0,a[row_index].pop(len(a[row_index])-1))
+        a = transpose(a)
+    else:
+        m = line.split('=')[1].split(' by ')
+        row_index = int(m[0])
+        num = int(m[1])
+
+        for i in range(0, num):
+            a[row_index].insert(0,a[row_index].pop(len(a[row_index])-1))
+
+total = 0
+for r in a:
+
+    for e in r:
+        if e == 0:
+            sys.stdout.write(' ')
+        else:
+            sys.stdout.write('#')
+        total+=e
+    sys.stdout.write('\n')
+
+print total
